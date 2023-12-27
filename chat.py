72636e0d15e630f2801f1b3460a0133502ae713f
Adnan Syed <adnansyed.ai@gmail.com>
2023-12-27 20:03:01 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -18,9 +18,7 @@ from config import OPENAI_API_KEY
 
 persist_directory = 'docs/chroma/chatbot/'
 
-embedding = OpenAIEmbeddings(api_key=OPENAI_API_KEY.get("key")
-
-
+embedding = OpenAIEmbeddings(api_key=OPENAI_API_KEY.get("key"))
 vectordb = Chroma(persist_directory=persist_directory,embedding_function=embedding)
 
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key=OPENAI_API_KEY.get("key"))
