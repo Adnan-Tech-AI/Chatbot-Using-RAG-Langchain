5f539750f7c6a78ad1591962a9b0eefe9c2426f4
Adnan Syed <adnansyed.ai@gmail.com>
2023-12-29 14:53:27 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -18,7 +18,7 @@ import streamlit.components.v1 as components
 
 openai_key = st.secrets["openai"]["key"]
 
-persist_directory = 'chatbot2/'
+persist_directory = 'docs/chroma/fulltimeclass/'
 
 embedding = OpenAIEmbeddings(api_key=openai_key)
 
